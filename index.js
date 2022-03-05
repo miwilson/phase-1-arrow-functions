634667947def0cc550bbea4314cd8dca4a1479b3
Michaela Wilson <+>
2022-03-05 16:55:03 -0700

    commit tree 880c364a99ac2181835c29113d943a6f32516251

---
+++ index.js
@@ -0,0 +1,14 @@
+const divide = (number1, number2) =>{ 
+    return 2000 / 100;
+}
+
+let x = 2
+const square = (x) =>{
+   return x ** 2;
+}
+
+let a = 3
+let b = 4
+const add = (a, b) =>{
+    return a + b;
+}
